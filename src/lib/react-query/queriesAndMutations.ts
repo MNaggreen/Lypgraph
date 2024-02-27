95d7a01fa3fb1b1eebe981e15031d0071752fd71
Nikolay Oreschenko <nickolas1594@mail.ru>
2024-02-27 18:50:50 +0300

    updated tsconfig

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -126,11 +126,11 @@ export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
     queryFn: getInfinitePosts,
-    getNextPageParam: (lastPage) => {      
-      if (lastPage && lastPage.documents.length === 0) {
+    getNextPageParam: (lastPage) => {          
+      if (lastPage && lastPage?.documents.length === 0) {
         return null
       }      
-      const lastId = lastPage?.documents[lastPage.documents.length - 1].$id
+      const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id
       return lastId
     },
   })
